da0f6cc155d5fdd9feec0d4cdea82a55fe7907b4
hans <hhofner@gmail.com>
2021-09-29 18:39:33 +0900

    Create setup_os.sh

---
+++ setup_os.sh
@@ -0,0 +1,18 @@
+echo "Setting up for MacOS"
+
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+brew install neovim
+brew install tmux
+brew install docker
+brew install docker-compose
+
+brew install --cask spotify
+brew install --cask firefox
+brew install --cask webstorm
+brew install --cask visual-studio-code
